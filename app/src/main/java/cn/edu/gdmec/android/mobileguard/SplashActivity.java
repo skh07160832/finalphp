5a607f4c094cde2c18a3bce618d85df7fd164c99
施凯焕 <31606844+skh07160832@users.noreply.github.com>
2017-12-12 08:38:13 +0800

    Update SplashActivity.java

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -17,6 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         
         
         
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
     }
